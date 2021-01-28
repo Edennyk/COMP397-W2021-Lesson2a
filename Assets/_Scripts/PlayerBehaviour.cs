555637a1342977b1ae8e69a3d20ebef1ad739ebc
Eden <edennyk@gmail.com>
2021-01-28 13:58:38 -0500

    completed simple FPS Character Controller

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    public float movementForce;
-    public float jumpForce;
-    public Rigidbody rigidBody;
+    //public float movementForce;
+    //public float jumpForce;
+    //public Rigidbody rigidBody;
+
+    public CharacterController controller;
+
+    public float maxSpeed = 10.0f;
+    public float gravity = -30.0f;
+    public float jumpHeight = 3.0f;
+
+    public Transform groundCheck;
+    public float groundRadius = 0.5f;
+    public LayerMask groundMask;
+
+    public Vector3 velocity;
     public bool isGrounded;
     
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+       
+        //rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame - once every 16.6666ms
@@ -20,7 +34,28 @@ public class PlayerBehaviour : MonoBehaviour
     // approximately updates 60 times per second = 60fps(frame per second)
     void Update()
     {
-        if(isGrounded)
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundRadius, groundMask);
+        
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2.0f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * maxSpeed * Time.deltaTime);
+
+        if(Input.GetButton("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        
+       /* if(isGrounded)
         {
             if (Input.GetAxisRaw("Horizontal") > 0)
             {
@@ -51,10 +86,15 @@ public class PlayerBehaviour : MonoBehaviour
                 // jumping
                 rigidBody.AddForce(Vector3.up * jumpForce);
             }
-        }
+        }*/
     }
 
-     void OnCollisionEnter(Collision other)
+     void OnDrawGizmos()
+     {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(groundCheck.position, groundRadius);
+     }
+    /* void OnCollisionEnter(Collision other)
      {
         if(other.gameObject.CompareTag("Ground"))
         {
@@ -76,5 +116,5 @@ public class PlayerBehaviour : MonoBehaviour
         {
             isGrounded = false;
         }
-    }
+    }*/
 }
